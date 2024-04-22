c3e2e3ed762abb4dcae34156ffe8627287bc2dee
Talshyn <Talshyn.com>
2024-04-22 09:51:23 +0500

    ld

--- lab10/1.py
+++ lab10/1.py
@@ -1,1 +1,26 @@
 #dfffff
+#ex1
+def my_function():
+  print("Hello from a function")
+
+#ex2
+def my_functio():
+  print("Hello from a function")
+my_functio()
+
+#ex3
+def my_functin(fname, lname):
+  print(fname)
+
+#ex4
+def my_functon(x):
+   return x+5
+
+#ex5
+def my_funcion(*kids):
+  print("The youngest child is " + kids[2])
+
+#ex6
+def my_funtion(**kid):
+  print("His last name is " + kid["lname"])
+
